580ae201f03a0dae13c78988f26baa8cd4e9d455
Issa <badosa2@yahoo.fr>
2025-11-12 21:48:17 -0300

    migration

--- bank/modele_de_donnees/schema.sql
+++ bank/modele_de_donnees/schema.sql
@@ -9,6 +9,7 @@
 -- Enable necessary extensions
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE EXTENSION IF NOT EXISTS "pg_trgm"; -- For full-text search with trigrams
 
 -- ============================================================================
 -- 1. COLLECTIVITÉS TERRITORIALES
